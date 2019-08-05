0eb735a907d316c763532f1254805a7b804f5f80
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-08-04 22:04:51 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Portifolio incoming</h1>
+      <GlobalStyle />
+    </BrowserRouter>
+  )
+}
+
+export default App;
