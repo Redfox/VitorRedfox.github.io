3a391b3d495220c088a434c5722469eef98756a5
VitorRedfox <vitor_pereira.2013@hotmail.com>
2020-09-12 13:36:40 -0300

    Updates

---
+++ precache-manifest.ee264b7b354db367784fa6f39adfc64f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b5bdc080dedf18d4ee168af9b8f12a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdee30ba54cbfbf2503e",
+    "url": "/static/js/2.b88456fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b88456fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75abe5e742f869654f97",
+    "url": "/static/js/main.a8ba04b5.chunk.js"
+  },
+  {
+    "revision": "076ff6dfc7a57b7aff8b",
+    "url": "/static/js/runtime-main.c57d4366.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Page/precache-manifest.f284b28a9d2b8b1bd5731157e762eeaf.js"
+  "/precache-manifest.ee264b7b354db367784fa6f39adfc64f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Page/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
