c77d82865aba11e79ac023f07e8362dd597fb306
VitorRedfox <vitor_pereira.2013@hotmail.com>
2020-09-12 12:13:03 -0300

    feat: config typescript setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+const App: React.FC = () => (
+   <BrowserRouter>
+    <div>Hello World</div>
+  </BrowserRouter>
+)
+
+export default App;
